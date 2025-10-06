18fddf9985d829b36d883df5421f0e219378b529
Shrabani Chakraborty <shrabanichakraborty83@gmail.com>
2025-10-06 11:21:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1365881464-e492e",
+  "appId": "1:242239554235:web:096b063a23667f9b245f9e",
+  "apiKey": "AIzaSyD1bw0_mqDFyYWNxtjSjzl-GO-EU6H5-2U",
+  "authDomain": "studio-1365881464-e492e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "242239554235"
+};
